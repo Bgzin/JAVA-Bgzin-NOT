46748ec1a9a4f17e7a10ce4d60e1d5e33c231799
Bgzin <123783840+Bgzin@users.noreply.github.com>
2023-06-12 16:17:01 +0000

    12/06/23 progressão na somativa

--- atividadeBanco/App.java
+++ atividadeBanco/App.java
@@ -1,9 +1,14 @@
 package atividadeBanco;
 
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+import POOExercicio2.agenda;
 import atividadeBanco.Pessoas.Pessoas;
 import atividadeBanco.Pessoas.pessoaFisica;
 import atividadeBanco.Pessoas.pessoaJuridica;
+import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) {
@@ -11,15 +16,30 @@ public class App {
     Pessoas pessoa01 = new Pessoas();
     pessoaFisica fisica01    =  new pessoaFisica();
     pessoaJuridica juridica01 = new pessoaJuridica();
-
-    
-    System.out.println("Primeiro vamos criar sua conta");
+    System.out.println("Seja bem-vindo ao nosso Register!");
     System.out.println("Voce é pessoa Juridica(1) ou Fisica(2)?");
     int pergunta=sc.nextInt();
     if(pergunta==1){
-    System.out.println("Seja muito bem vindo ao nosso Registro digite seus dados abaixo");
+        pessoaJuridica RegistrosJuridico[] = new pessoaJuridica [2];
+     for (int i = 0; i < RegistrosJuridico.length; i++) {
+        //
+        RegistrosJuridico[i]= new pessoaJuridica();
+        //preencher os dados do cliente
+        RegistrosJuridico[i].setNomeConta(JOptionPane.showInputDialog("Nome: "));
+        RegistrosJuridico[i].setNumeroConta(JOptionPane.showInputDialog("Numero da Conta: "));
+        RegistrosJuridico[i].setCNPJ(JOptionPane.showInputDialog("CPNJ: "));
+        RegistrosJuridico[i].setSaldoConta(JOptionPane.showInputDialog("Deposito Inicial R$: "));
+     }
+
+    
+     
+
+
     }
+ 
+    }
+
     
     
     }
-}
+
