e1d30b9f09cf0ef6c729b645280f3099555a9159
Bgzin <123783840+Bgzin@users.noreply.github.com>
2023-06-15 00:20:02 -0300

    15/06 mudanças

--- atividadeBanco/App.java
+++ atividadeBanco/App.java
@@ -12,7 +12,7 @@ import javax.swing.JOptionPane;
 
 //SE APERTAR DIRETO NA OPÇÃO 2 ANTES DE SE REGISTRAR "VOCE NAO PODE CONSULTAR ALGO QUE NAO ESTA REGISTRADO"  FINAL DO CODE
 public class App {
-   public static void main(String[] args) {
+   public static void main(String[] args){
       Scanner sc = new Scanner(System.in);
       Pessoas pessoa01 = new Pessoas();
       pessoaFisica fisica01 = new pessoaFisica();
@@ -27,15 +27,15 @@ public class App {
          int acao = sc.nextInt();
          switch (acao) {
             case 1:
-               Registro(sc);
+               Registro(sc.nextInt());
                break;
             case 2:
-               // ConsultePJ(sc);
+               ConsultePJ(sc.nextInt());
                break;
             case 3:
-               
+               ConsultePF(sc.nextInt()); 
                break;
-               case 4:
+            case 4:
                ligado = false;
                break;
 
@@ -45,8 +45,9 @@ public class App {
          }
       }
    }
+   
 
-   public static void Registro(Scanner sc) {
+   public void Registro(Scanner sc) {
       System.out.println("Seja bem-vindo ao nosso Register!");
       System.out.println("Voce é pessoa Juridica(1) ou Fisica(2)?");
       int pergunta = sc.nextInt();
@@ -72,40 +73,97 @@ public class App {
          RegistrosFisica[contaPF].setSaldoConta(0);
          System.out.println("Pessoa Juridica: " + RegistrosFisica[contaPF].toString());
       }
+      }
 
-   }
+      
 
-   public static void ConsultePJ(Scanner sc) {
+   public  void ConsultePJ(Scanner sc) {
       System.out.println("Essa aba é especialmente para Consultar pessoas Juridicas, Prossiga com as seguintes opões");
       boolean consultandoPJ = true;
       while (consultandoPJ) {
          System.out.println("Informe a ação Desejada"
-            + "\n1-Verificar Saldo"
-            + "\n2-Saque"
-            + "\n3-Depósito;"
-            + "\n4-Empréstimo;"
-            + "\n5-Sair");
-
+               + "\n1-Verificar Saldo"
+               + "\n2-Saque"
+               + "\n3-Depósito;"
+               + "\n4-Empréstimo;"
+               + "\n5-Sair");
          int acao = sc.nextInt();
-         switch (acao) {
-            case 1:
-            System.out.println("Seu saldo é de R$: "+pessoaJuridica.getSaldoConta());
-               break;
-            case 2:
-               
-               break;
-            case 3:
-               break;
-            case 4:
-               break;
-            case 5:
-              consultandoPJ=false;
-               break;
-            default:
-               System.out.println("Opção Invalida");
-               break;
+         if (acao == 1) {
+            System.out.println("Seu saldo é de R$: " + juridica01.getSaldoConta());
+         } else if (acao == 2) {
+            RegistrosJuridico[i].saque();
+         } else if (acao == 3) {
+            RegistroJuridico[i].deposito();
+         } else if (acao == 4) {
+
+            System.out.println("Escolha o valor do seu Emprestimo, minimo de R$10,000 maximo de R$120.000,00");
+
+            double valorEmprestimo = sc.nextDouble();
+
+            double emprestimoMin = 10000;
+
+            double emprestimoMax = 1200000;
+
+            double jurosEmprest = 0.04;
+            if (valorEmprestimo >= emprestimoMin && valorEmprestimo < emprestimoMax) {
+               double valorTotal = jurosEmprest * valorEmprestimo;
+               System.out.println("o valor solicitado do seu emprestimo foi aprovado" + valorTotal + pessoa01.saldoConta);
+            } else {
+               System.out.println("o valor solicitado não foi aprovado");
+
+           
          }
+      } else if(acao == 5){
+          consultandoPJ=false;
+      } else { 
+         System.out.println("A ação requerida nao existe!! tente novamente");
       }
+      }
+   }
+
+   public  void ConsultePF(Scanner sc) {
+      System.out.println("Essa aba é especialmente para Consultar pessoas Juridicas, Prossiga com as seguintes opões");
+      boolean consultandoPF = true;
+      while (consultandoPF) {
+         System.out.println("Informe a ação Desejada"
+               + "\n1-Verificar Saldo"
+               + "\n2-Saque"
+               + "\n3-Depósito;"
+               + "\n4-Empréstimo;"
+               + "\n5-Sair");
+         int acao2 = sc.nextInt();
+         if (acao2 == 1) {
+            System.out.println("Seu saldo é de R$: " +fisica01.getSaldoConta());
+         } else if (acao2 == 2) {
+            RegistrosJuridico[i].saque();
+         } else if (acao2 == 3) {
+            RegistroJuridico[i].deposito();
+         } else if (acao2 == 4) {
+
+            System.out.println("Escolha o valor do seu Emprestimo, minimo de R$10,000 maximo de R$120.000,00");
+
+            double valorEmprestimoPF = sc.nextDouble();
+
+            double emprestimoMinPF = 1000;
+
+            double emprestimoMaxPF = 24000;
+
+            double jurosEmprest = 0.04;
+            if (valorEmprestimoPF >= emprestimoMinPF && valorEmprestimoPF < emprestimoMaxPF) {
+               double valorTotalPF = jurosEmprest * valorEmprestimoPF;
+               System.out.println("o valor solicitado do seu emprestimo foi aprovado" + valorTotalPF + fisica01.saldoConta);
+               
+            } else {
+               System.out.println("o valor solicitado não foi aprovado");     
+         }
+      } else if(acao == 5){
+          consultandoPF=false;
+      } else { 
+         System.out.println("A ação requerida nao existe!! tente novamente");
+      }
+   }
 
    }
 }
+    
+
