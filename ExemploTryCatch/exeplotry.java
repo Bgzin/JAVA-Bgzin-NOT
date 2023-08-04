4d17f9355eb0bfff81fc56f11eb859b78a5244dd
Bgzin <rogerbiagioni06@gmail.com>
2023-08-03 21:55:39 -0300

    03/08

--- ExemploTryCatch/exeplotry.java
+++ ExemploTryCatch/exeplotry.java
@@ -2,20 +2,70 @@ package ExemploTryCatch;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class exeplotry {
     public static void main(String[] args) {
         boolean tente = true;
         while(tente){
-        Scanner s = new Scanner(System.in);
-        try {
-        System.out.print("Digite um valor inteiro..:");
-        int numero1 = s.nextInt();
-        System.out.print("Digite um valor inteiro..:");
-        int numero2 = s.nextInt();
-        System.out.println(numero1+ " + " + numero2 + " = " + (numero1+numero2));
-        } catch (Exception e) {
-        System.out.println("ERRO - Valor digitado nao e um numero inteiro! tente novamente");
-        }       
+        // escolha uma opção
+        int escolha = Integer.parseInt(JOptionPane.showInputDialog("Digite\n1 para somar\n2 para subtrair\n3 para multiplicar \n4 para dividir")); 
+        if(escolha==1){
+            try {
+        int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Numero Inteiro:"));
+        int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro numero Inteiro: "));
+        JOptionPane.showMessageDialog(null, numero1+ " + " + numero2 + " = " + (numero1+numero2));
+        tente= false;
+
+        }catch (NumberFormatException erro1) {
+            JOptionPane.showMessageDialog(null, "Erro - Valor digitado não é um inteiro"
+                                                                +"\nDigite novamente");
+        } catch (Exception erro2) {
+            JOptionPane.showMessageDialog(null, "Aconteceu um erro"
+                                                                +"\nDigite novamente");
         }
-    }
-}
+        }else if(escolha==2){
+              try {
+        int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Numero Inteiro:"));
+        int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro numero Inteiro: "));
+        JOptionPane.showMessageDialog(null, numero1+ " + " + numero2 + " = " + (numero1-numero2));
+        tente= false;
+
+        }catch (NumberFormatException erro1) {
+            JOptionPane.showMessageDialog(null, "Erro - Valor digitado não é um inteiro"
+                                                                +"\nDigite novamente");
+        } catch (Exception erro2) {
+            JOptionPane.showMessageDialog(null, "Aconteceu um erro"
+                                                                +"\nDigite novamente");
+        }
+        }else if(escolha==3){
+              try {
+        int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Numero Inteiro:"));
+        int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro numero Inteiro: "));
+        JOptionPane.showMessageDialog(null, numero1+ " + " + numero2 + " = " + (numero1*numero2));
+        tente= false;
+
+        }catch (NumberFormatException erro1) {
+            JOptionPane.showMessageDialog(null, "Erro - Valor digitado não é um inteiro"
+                                                                +"\nDigite novamente");
+        } catch (Exception erro2) {
+            JOptionPane.showMessageDialog(null, "Aconteceu um erro"
+                                                                +"\nDigite novamente");
+        }
+        }else{
+              try {
+        int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Numero Inteiro:"));
+        int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro numero Inteiro: "));
+        if(numero1>=0 || numero2>0){
+          JOptionPane.showMessageDialog(null, numero1+ " + " + numero2 + " = " + (numero1/numero2));
+        tente= false;
+        }
+        }
+              catch (NumberFormatException erro1) {
+            JOptionPane.showMessageDialog(null, "Erro - Valor digitado não é um inteiro"
+                                                                +"\nDigite novamente");
+        } catch (ArithmeticException erro2) {
+            JOptionPane.showMessageDialog(null, "Não se pode dividir por 0"
+                                                                +"\nDigite novamente");
+}}}}}
+
