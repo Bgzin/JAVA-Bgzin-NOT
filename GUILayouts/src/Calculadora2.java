d85a13cf1e521879f2d4b7786857f4051d85d4c8
Bgzin <123783840+Bgzin@users.noreply.github.com>
2023-09-21 17:32:49 -0300

    comentarios e algumas correçoes 21/09

--- GUILayouts/src/Calculadora2.java
+++ GUILayouts/src/Calculadora2.java
@@ -3,7 +3,6 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,115 +12,137 @@ import javax.swing.SwingUtilities;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+// Classe que representa a Calculadora2, que é um JPanel (um painel em uma interface gráfica)
 public class Calculadora2 extends JPanel {
+    // Declaração de variáveis  (componentes gráficos)
     JTextField textFieldHora;
     JTextField textFieldMinuto;
     JTextField textFieldSegundo;
+    JLabel labelHoraOriginal;
 
+    // Construtor da classe Calculadora2
     public Calculadora2() {
-        super();
-        this.setLayout(new GridBagLayout());
+        // Configuração do layout do painel principal usando GridBagLayout
+        super(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 10, 10, 10); // Aumentei o espaçamento
+        gbc.insets = new Insets(10, 10, 10, 10); // Espaçamento entre os elementos
 
-        // Título
+        // Criação do rótulo (label) do título da calculadora
         JLabel titleLabel = new JLabel("Conversor de Tempo");
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 36)); // Aumentei o tamanho da fonte
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 2;
-        this.add(titleLabel, gbc);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 36)); // Configuração da fonte
+        gbc.gridx = 0; // Posição na grade (coluna)
+        gbc.gridy = 0; // Posição na grade (linha)
+        gbc.gridwidth = 2; // Largura da célula na grade (ocupa 2 colunas)
+        add(titleLabel, gbc); // Adiciona o rótulo ao painel
 
-        // Painel para os campos de entrada
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        // Criação de um painel secundário com layout FlowLayout para os campos de entrada
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-        textFieldHora = new JTextField(10); // Aumentei o tamanho dos campos
+        // Criação dos campos de texto para horas, minutos e segundos
+        textFieldHora = new JTextField(10);
         textFieldMinuto = new JTextField(10);
         textFieldSegundo = new JTextField(10);
 
-        // Rótulos e campos de entrada
-        panel.add(new JLabel("Horas:"));
-        panel.add(textFieldHora);
-        panel.add(new JLabel("Minutos:"));
-        panel.add(textFieldMinuto);
-        panel.add(new JLabel("Segundos:"));
-        panel.add(textFieldSegundo);
+        // Criação do rótulo "Horas originais" e configuração de seu texto
+        labelHoraOriginal = new JLabel("Horas originais:");
+        panel.add(labelHoraOriginal); // Adiciona o rótulo ao painel secundário
+        panel.add(textFieldHora); // Adiciona o campo de texto de horas ao painel
+        panel.add(new JLabel("Minutos:")); // Adiciona um rótulo "Minutos:"
+        panel.add(textFieldMinuto); // Adiciona o campo de texto de minutos ao painel
+        panel.add(new JLabel("Segundos:")); // Adiciona um rótulo "Segundos:"
+        panel.add(textFieldSegundo); // Adiciona o campo de texto de segundos ao painel
 
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.gridwidth = 2;
-        this.add(panel, gbc);
+        add(panel, gbc); // Adiciona o painel secundário com os campos de entrada ao painel principal
 
-        // Painel para botões
+        // Criação de um painel secundário com layout FlowLayout para os botões
         JPanel buttonPanel = new JPanel(new FlowLayout());
 
-        // Botão de conversão
-        JButton converterButton = new JButton("Converter");
-        converterButton.setFont(new Font("Arial", Font.PLAIN, 18)); // Aumentei o tamanho da fonte
-        converterButton.addActionListener(new ActionListener() {
+        // Criação e configuração do botão "Converter"
+        JButton converterButton = createButton("Converter", 14, new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                try {
-                    String horaStr = textFieldHora.getText();
-                    String minutoStr = textFieldMinuto.getText();
-                    String segundoStr = textFieldSegundo.getText();
-
-                    // Verifica se os campos estão vazios
-                    if (horaStr.isEmpty()) horaStr = "0";
-                    if (minutoStr.isEmpty()) minutoStr = "0";
-                    if (segundoStr.isEmpty()) segundoStr = "0";
-
-                    int horas = Integer.parseInt(horaStr);
-                    int minutos = Integer.parseInt(minutoStr);
-                    int segundos = Integer.parseInt(segundoStr);
-                    //converter horas para minutos 
-                    int totalMinutos = (horas * 60) + minutos;
-
-                    // Converter minutos para segundos 
-                    int totalSegundos = (totalMinutos * 60) + segundos;
-
-                    textFieldMinuto.setText(String.valueOf(totalMinutos));
-                    textFieldSegundo.setText(String.valueOf(totalSegundos));
-
-                    // Limpa o campo de horas
-                    textFieldHora.setText("");
-                } catch (NumberFormatException ex) {
-                    textFieldHora.setText("Erro");
-                    textFieldMinuto.setText("Erro");
-                    textFieldSegundo.setText("Erro");
-                }
+                converterTempo(); // Chama o método converterTempo() quando o botão é clicado
             }
         });
 
-        // Botão de limpar
-        JButton limparButton = new JButton("Limpar");
-        limparButton.setFont(new Font("Arial", Font.PLAIN, 18)); // Aumentei o tamanho da fonte
-        limparButton.addActionListener(new ActionListener() {
+        // Criação e configuração do botão "Limpar"
+        JButton limparButton = createButton("Limpar", 14, new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                textFieldHora.setText("");
-                textFieldMinuto.setText("");
-                textFieldSegundo.setText("");
+                limparCampos(); // Chama o método limparCampos() quando o botão é clicado
             }
         });
 
+        // Adiciona os botões ao painel secundário de botões
         buttonPanel.add(converterButton);
         buttonPanel.add(limparButton);
+
         gbc.gridx = 0;
         gbc.gridy = 2;
         gbc.gridwidth = 2;
-        this.add(buttonPanel, gbc);
+        add(buttonPanel, gbc); // Adiciona o painel secundário de botões ao painel principal
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                JFrame frame = new JFrame("Calculadora2");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.add(new Calculadora2());
-                frame.pack();
-                frame.setSize((int) (frame.getWidth() * 1.4), (int) (frame.getHeight() * 1.4)); // Aumentei o tamanho da janela
-                frame.setVisible(true);
-            }
+    // Método para criar um botão com um tamanho de fonte específico e um ouvinte de ação
+    private JButton createButton(String text, int fontSize, ActionListener listener) {
+        JButton button = new JButton(text);
+        button.setFont(new Font("Arial", Font.PLAIN, fontSize)); // Configuração da fonte do botão
+        button.addActionListener(listener); // Adiciona o ouvinte de ação ao botão
+        return button; // Retorna o botão criado
+    }
+
+    // Método para realizar a conversão de tempo
+    private void converterTempo() {
+        // Obtém os valores dos campos de texto para horas, minutos e segundos
+        String horaStr = textFieldHora.getText();
+        String minutoStr = textFieldMinuto.getText();
+        String segundoStr = textFieldSegundo.getText();
+
+        // Verifica se os campos estão vazios e atribui valores padrão se estiverem
+        if (horaStr.isEmpty()) horaStr = "0";
+        if (minutoStr.isEmpty()) minutoStr = "0";
+        if (segundoStr.isEmpty()) segundoStr = "0";
+
+        // Converte os valores de texto em números inteiros
+        int horas = Integer.parseInt(horaStr);
+        int minutos = Integer.parseInt(minutoStr);
+        int segundos = Integer.parseInt(segundoStr);
+
+        // Atualiza o rótulo de "Horas originais" com o valor das horas digitadas
+        labelHoraOriginal.setText("Horas originais: " + horaStr);
+
+        // Realiza a conversão de horas e minutos para minutos totais
+        int totalMinutos = (horas * 60) + minutos;
+
+        // Realiza a conversão de minutos totais e segundos para segundos totais
+        int totalSegundos = (totalMinutos * 60) + segundos;
+
+        // Atualiza os campos de texto de minutos e segundos com os valores convertidos
+        textFieldMinuto.setText(String.valueOf(totalMinutos));
+        textFieldSegundo.setText(String.valueOf(totalSegundos));
+
+        // Limpa o campo de texto de horas
+        textFieldHora.setText("");
+    }
+
+    //  limpar os campos de entrada e o rótulo de "Horas originais"
+    private void limparCampos() {
+        textFieldHora.setText("");
+        textFieldMinuto.setText("");
+        textFieldSegundo.setText("");
+        labelHoraOriginal.setText("Horas originais:");
+    }
+
+    //  iniciar a aplicação
+       public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Calculadora2");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(new Calculadora2());
+            frame.pack();
+            frame.setSize((int) (frame.getWidth() * 1.4), (int) (frame.getHeight() * 1.4));
+            frame.setVisible(true);
         });
     }
 }
